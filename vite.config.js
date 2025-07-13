ae648364e918dd8bb744e2b3b7c98973a547883f
ElishaMlyasende <155617558+ElishaMlyasende@users.noreply.github.com>
2025-07-13 12:57:02 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Optional: change output folder if you want
-    // outDir: 'build',
-  },
+});
 });
